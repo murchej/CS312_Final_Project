4833619013f502353978f82a9fc4b39176f1cbb6
Joe Murche <jmurche@yahoo.com>
2023-06-14 13:04:27 -0700

    Update

--- CS312_Final_Project/main.tf
+++ CS312_Final_Project/main.tf
@@ -12,6 +12,9 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
+  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files      = ["~/.aws/config"]
+  profile                  = "default"
 }
 
 
